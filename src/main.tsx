13545aa826ecf08b41f5f53161c03d848c3dd297
ShubhamVerma77 <sv8144906@gmail.com>
2026-06-02 15:36:20 +0530

    Add Mantine dependencies and configure PostCSS
    - Updated package.json and package-lock.json to include @mantine/core, @mantine/hooks, and related PostCSS packages.
    - Created postcss.config.cjs for Mantine styling.
    - Refactored App component to utilize Mantine Button and removed unused code.
    - Wrapped App component in MantineProvider in main.tsx for theme support.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+     <MantineProvider>
     <App />
-  </StrictMode>,
+
+      </MantineProvider>
+  </StrictMode>
 )
